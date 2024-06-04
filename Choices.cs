5187c8308c1185b4375313128cdff2c38f89f863
as9233 <as6500327@gmail.com>
2024-06-04 15:27:20 +0530

    Commit

---
+++ Choices.cs
@@ -0,0 +1,12 @@
+//3 rooms silver , gold , bronz prices accordingly choice of the room , clac acc to no. of days , name , gov id , phone no. 
+//phase_1- 3 mai se ek 1 room book
+//phase_2 - 3 mai se 2 room book
+//phase_3 - all 3
+
+namespace Problem
+{
+    class Choices
+    {
+        
+    }
+}
